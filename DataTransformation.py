686d9f5fe35be6e522246c031fa2f3f28113e58a
chengmarc <last@live.ca>
2025-02-09 19:27:56 -0500

    minor change

--- DataTransformation.py
+++ DataTransformation.py
@@ -23,17 +23,9 @@ def prepare_data():
 
     data['Date'] = pd.to_datetime(data['time'])
     data.set_index('Date', inplace=True)
-
-    data['Difficulty'] = np.log(data['DiffLast'])
-    data['Transaction Count'] = np.log(data['TxCnt'])
-    data['Active Addresses Count'] = np.log(data['AdrActCnt'])
-
-    data['30 Day Active Supply'] = np.log(data['SplyAct30d'])-10
-    data['1 Year Active Supply'] = np.log(data['SplyAct1yr'])-10
-    data['CurrentSupply'] = np.log(data['SplyCur'])-10
     data['LogPriceUSD'] = np.log(data['PriceUSD'])
 
-    data = data[['Difficulty', 'Transaction Count', 'Active Addresses Count', '30 Day Active Supply', '1 Year Active Supply', 'CurrentSupply', 'LogPriceUSD']]
+    data = data[['LogPriceUSD']]
     data.fillna(0, inplace=True)
     
     return data
